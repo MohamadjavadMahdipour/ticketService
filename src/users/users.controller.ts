94a431747bed33ac2c9e95a5fd33b11579c8cb4e
javad <mahdipourjavad1379@gmail.com>
2025-08-25 15:43:54 +0330

    nn

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('create-test')
+  @ApiOperation({ summary: 'Create a test user' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: { type: 'string' },
+        phone_number: { type: 'string' },
+        username: { type: 'string' },
+        password: { type: 'string' },
+      },
+      required: ['name', 'phone_number', 'username', 'password'],
+    },
+  })
+  async createTestUser(@Body() body: any) {
+    return this.usersService.createTestUser(body.name, body.phone_number, body.username, body.password);
+  }
+
+  @Get('all')
+  @ApiOperation({ summary: 'Get all users' })
+  async getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createTestUser(name: string, phone_number: string, username: string, password: string) {
+    const user = this.userRepository.create({
+      name,
+      phone_number,
+      username,
+      password,
+      role: 'employee',
+      is_active: true,
+    });
+    return this.userRepository.save(user);
+  }
+
+  async getAllUsers() {
+    return this.userRepository.find();
+  }
+}
