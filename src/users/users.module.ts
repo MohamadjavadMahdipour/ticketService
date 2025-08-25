7f0b9924138e0f83df854952f01b59b5074d57da
javad <mahdipourjavad1379@gmail.com>
2025-08-25 15:54:05 +0330

    nice

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Ticket } from 'src/tickets/entities/ticket.entity/ticket.entity';
+import { Chat } from 'src/chats/entities/chat.entity/chat.entity';
+import { File } from 'src/files/entities/file.entity/file.entity';
+import { User } from './entities/user.entity/user.entity';
 
 @Module({
+   imports: [
+      TypeOrmModule.forFeature([Ticket, Chat, File, User]), // <-- important
+    ],
   providers: [UsersService],
   controllers: [UsersController]
 })
